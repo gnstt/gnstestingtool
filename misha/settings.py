903a13a60b818db0dffeec1abed17e38487145c9
gnstt <teamfm@ukr.net>
2018-10-06 14:39:23 +0300

    update settings.py - static root

--- misha/settings.py
+++ misha/settings.py
@@ -106,8 +106,9 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/gnstt/gnstestingtool/app/static'
 
 #STATICFILES_DIRS = (
 #                os.path.join(PROJECT_DIR,'staticfiles'), # if your static files folder is named "staticfiles"
